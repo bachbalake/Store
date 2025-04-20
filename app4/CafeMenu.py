c2a6e2818e100e492358f574132dc24c47373649
Tamanna Dhiman <DhimanTS@cardiff.ac.uk>
2025-04-20 13:38:36 +0100

    Added working basket mechanisms. Also created database with basket, recipes, cafe menu, and ingredients. Calculates carbon footprint

--- app4/CafeMenu.py
+++ app4/CafeMenu.py
@@ -1,4 +1,4 @@
-from app4 import app, db, open_cafe
+from app4 import app, db, open_cafe, recipe, ingredients, ingredient_recipe_amount
 
 
 cafe_menu = [
@@ -7,11 +7,105 @@ cafe_menu = [
     { "name": "Irish Coffee", "price": 7.50, "description": "An uplifting caffeinated alcoholic drink made of Irish whiskey, coffee and sweetners.", "image" : "irish coffee.jpg" },
     { "name": "Affogato", "price": 4.75, "description": "A sweet treat made by pouring a shot of espresso over a scoop of vanilla ice cream", "image" : "affogato.jpg" }
 ]
+recipes_data = [
+   {
+      "name": "Ca Phe Muoi", 
+      "ingredients" : [
+         {"name" : "Brew Coffee", "amount" : 235},
+         {"name" : "Heavy Cream", "amount" : 89},
+         {"name" : "Sugar", "amount" : 30},
+         {"name" : "Salt", "amount" : 3},
+         {"name" : "Milk", "amount" : 120 },
+         {"name" : "Cocoa powder", "amount" : 30}
+      ]
+   },
+   {
+      "name": "Matcha Tonic", 
+      "ingredients" : [
+         {"name" : "Strawberry", "amount" : 83},
+         {"name" : "Matcha Powder", "amount" : 8},
+         {"name" : "Sugar", "amount" : 15},
+         {"name" : "Tonic Water", "amount" :235 }
+      ]
+   },
+   {
+      "name": "Irish Coffee", 
+      "ingredients" : [
+         {"name" : "Brew Coffee", "amount" : 470},
+         {"name" : "Heavy Cream", "amount" : 320},
+         {"name" : "Sugar", "amount" : 17},
+         {"name" : "Irish Whiskey", "amount" : 90},
+         {"name" : "Chocolate", "amount" : 110}
+     ]
+   },
+   {
+      "name": "Affogato", 
+      "ingredients" : [
+         {"name" : "Espresso", "amount" : 44},
+         {"name" : "Vanilla Ice Cream", "amount" : 240},
+         {"name" : "Amaretto", "amount" : 20},
+         {"name" : "Chocolate", "amount" : 15}
+      ]
+   }
+]
+
+#Carbon Footprint (g CO₂e per g/ml)
+# Convert to kg when required?
+
+ingredient_carbon_footprint = [
+   {"name" : "Brew Coffee", "carbon_footprint" : 0.25},
+   {"name" : "Heavy Cream", "carbon_footprint" : 3.77},
+   {"name" : "Sugar", "carbon_footprint" : 0.24},
+   {"name" : "Salt", "carbon_footprint" : 0.20},
+   {"name" : "Milk", "carbon_footprint" : 1},
+   {"name" : "Cocoa powder", "carbon_footprint" : 1.40},
+   {"name" : "Strawberry", "carbon_footprint" : 0.22},
+   {"name" : "Matcha Powder", "carbon_footprint" : 1.90},
+   {"name" : "Tonic Water", "carbon_footprint" : 0.30},
+   {"name" : "Irish Whiskey", "carbon_footprint" : 2},
+   {"name" : "Chocolate", "carbon_footprint" : 5},
+   {"name" : "Espresso", "carbon_footprint" : 0.28},
+   {"name" : "Vanilla Ice Cream", "carbon_footprint" : 1.50},
+   {"name" : "Amaretto", "carbon_footprint" : 2}
+]
+
 
 
 with app.app_context():
    db.create_all()
+
    for item in cafe_menu:
-      item_entry = open_cafe(name = item["name"], price = item["price"], description = item["description"], image = item["image"])
-      db.session.add(item_entry)
+      item_entry = open_cafe.query.filter_by(name=item["name"]).first()
+      if not item_entry:
+         item_entry = open_cafe(name = item["name"], price = item["price"], description = item["description"], image = item["image"])
+         db.session.add(item_entry) 
    db.session.commit()
+
+   for ingredient in ingredient_carbon_footprint:
+      ingredient_entry = ingredients.query.filter_by(name = ingredient['name']).first()
+      if not ingredient_entry:
+         ingredient_entry = ingredients(name = ingredient["name"], carbon_footprint = ingredient["carbon_footprint"])
+         db.session.add(ingredient_entry)
+   db.session.commit()
+
+   for recipe_data in recipes_data:
+      item = open_cafe.query.filter_by(name = recipe_data["name"]).first()
+      if not item:
+         print(f"No item found for recipe {recipe_data['name']}")
+         continue
+
+      recipe_entry = recipe(item_id = item.id)
+      db.session.add(recipe_entry)
+      db.session.flush()
+
+      for ingredient in recipe_data["ingredients"]:
+         ingredient_entry = ingredients.query.filter_by(name = ingredient["name"]).first()
+         if not ingredient_entry:
+            print(f"Ingredient not found {ingredient['name']}")
+            continue
+
+         connection = ingredient_recipe_amount(recipe_id = recipe_entry.id, ingredient_id = ingredient_entry.id, amount = ingredient["amount"])
+         db.session.add(connection)
+   db.session.commit()
+
+
--- app4/app4.py
+++ app4/app4.py
@@ -4,6 +4,7 @@ from wtforms import IntegerField, SubmitField
 from wtforms.validators import NumberRange, DataRequired
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy.ext.hybrid import hybrid_property
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -12,22 +13,85 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
 db = SQLAlchemy(app)
 
 class open_cafe(db.Model):
-    __tablename__ = 'Cafe Menu'
+    __tablename__ = 'cafe_menu'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column (db.String(16), index = True, unique = True, nullable = False)
     price = db.Column (db.Float, nullable = False)
     description = db.Column(db.String)
     image  = db.Column(db.String)
 
+#Maybe create a page that allows the user to buy the items and then displays all the pictures in a fun way to represent the items bought.It then resets the basket
+
+# Want to create a basket for each user where user can add items and amount. Add constraints to the amount they can add, if item already exists then just increment the value there 
+class basket(db.Model):
+    __tablename__ = 'user_basket'
+    id = db.Column(db.Integer, primary_key = True)
+    item_id = db.Column (db.Integer, db.ForeignKey('cafe_menu.id')) 
+    item_quantity = db.Column (db.Integer, nullable = False)
+    item = db.relationship('open_cafe', backref = 'baskets')
+
+    @hybrid_property
+    def total_price(self):
+        return self.item_quanity * self.item.price
+# Want to create a login page, or a sign up page
+# Each user has a unique id, name, password and basket_id
+# Options to view the user account at all times, reset password?, delete account, try emailing for code?
+
+
+class recipe (db.Model):
+    __tablename__ = 'all_recipes'
+    id = db.Column(db.Integer, primary_key = True)
+    item_id = db.Column (db.Integer, db.ForeignKey('cafe_menu.id'))
+    ingredients = db.relationship('ingredient_recipe_amount', back_populates = 'recipe')
+
+    @hybrid_property
+    def total_carbon_footprint(self):
+        total = 0
+        for ingredient_recipe in self.ingredients:
+            total += ingredient_recipe.amount * ingredient_recipe.ingredient.carbon_footprint
+        return total
+
+
+class ingredients (db.Model):
+    __tablename__ = 'all_ingredients'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), unique = True, nullable = False)
+    carbon_footprint = db.Column (db.Integer)
+    ingredient_recipe_amounts = db.relationship('ingredient_recipe_amount', back_populates='ingredient') 
+
+class ingredient_recipe_amount(db.Model):
+    __tablename__ = 'ingredients_for_each_recipe'
+    id = db.Column (db.Integer, primary_key = True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey ('all_recipes.id'))
+    ingredient_id = db.Column (db.Integer, db.ForeignKey ('all_ingredients.id'))
+    amount = db.Column (db.Float)
+    recipe = db.relationship('recipe', back_populates = 'ingredients')
+    ingredient = db.relationship('ingredients', back_populates = 'ingredient_recipe_amounts')
+
+
+"""
+class user(db.Model):
+    __tablename__ = 'User Information'
+    id = db.Column (db.Integer, primary_key = True)
+    name = db.Column (db.String(45), index = True, unique = True, nullable = False)
+    password = db.Column (db.String(24), unique = True, nullable = False) # Make it so the password is hidden?
+    basket_id = db.Column (db.Integer) # It is a foreign key, the parent key is a basket record. I want to create a new basket when a new user is created
+class user_list (db.Model):
+    __tablename__ = 'Users List'
+    user_id = db.Column (db.Integer) # This is a foreign key, its parent is id from user
+    purchase_amount = db.Column (db.Float) # Using the basket it calculates through the amount bought and price the total money spent by the user
+"""
 
 class Basket_form(FlaskForm):
     amount = IntegerField('Add the desired amount', validators = [DataRequired(), NumberRange(min = 1, max = 15)])
     buy = SubmitField('Buy')
 
+
 @app.route('/')
 def galleryPage():
     cafe_menu = open_cafe.query.all()
-    return render_template('index.html',cafe_menu = cafe_menu)
+    cafe_recipes = recipe.query.all()
+    return render_template('index.html',cafe_menu = cafe_menu, cafe_recipes = cafe_recipes)
 
 
 @app.route('/product_page/<int:item>', methods = ['GET', 'POST'])
@@ -35,11 +99,41 @@ def singleProductPage(item):
     purchase_form = Basket_form()
     selected_item = open_cafe.query.get_or_404(item)
     if purchase_form.validate_on_submit():
-        return render_template('PurchaseProduct.html', cafe_item = selected_item, basket = purchase_form.amount.data)
+        print("Form validated! Processing basket...")
+        quantity = purchase_form.amount.data
+        item_entry = basket.query.filter_by(item_id = selected_item.id).first()
+
+        if item_entry:
+            item_entry.item_quantity += quantity
+        else:
+            item_entry = basket(item_id = selected_item.id, item_quantity = quantity)
+            db.session.add(item_entry)
+        db.session.commit()
+        return render_template('PurchaseProduct.html', item=selected_item, quantity_added = quantity, basket_entry=item_entry) 
     else: 
         return render_template('SingleProduct.html', cafe_item = selected_item, basket = purchase_form)
+    
+# @app.route('user/basket')
+@app.route('/basket')
+def basketPage():
+    user_basket = basket.query.all()
+    return render_template('ViewBasket.html', user_basket = user_basket)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
 
 
+"""
+Allows for calculation of total price in SQL
+@total_price.expression
+def total_price(cls):
+    return cls.item_quantity * open_cafe.price
+"""
+"""{% extends "base.html" %}
+<!DOCTYPE html>
+<body>
+   <h1> {{ item.name }}</h1>
+   <h3>Items Added: </h3>
+   <p> {{ basket_entry.item_quantity }} </p>
+</body>"""
